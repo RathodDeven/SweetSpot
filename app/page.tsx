dd46236bd6e64313c3a1f2f6e9e7f559f7c464c9
Rathod Deven <devenrathodrd@gmail.com>
2024-02-13 16:16:57 +0530

    modifying text

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import './globals.css'
-import Image from 'next/image'
 import { APP_NAME } from '../utils/config'
 import CelebrationIcon from '@mui/icons-material/Celebration'
 import { Button } from '@mui/material'
@@ -10,6 +9,11 @@ export default function Home() {
   return (
     <div className="h-full centered-col">
       <div className="text-xl font-bold py-10">Your Project : {APP_NAME} </div>
+      <div className="text-xl">
+        Start editing by modifying{' '}
+        <code className="text-red-400">components/wrappers/UILayout.tsx</code> &{' '}
+        <code className="text-red-400">app/page.tsx</code>
+      </div>
       <div className="text-xl font-bold py-10 between-row gap-x-2">
         Mui Icon :{' '}
         <div className="text-blue-400">
