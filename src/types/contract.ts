eb999131eebe7b952e410e5de3d14e8cdd50d772
Rathod Deven <devenrathodrd@gmail.com>
2024-12-10 16:14:40 +0530

    fix: temp deploy errors

--- src/types/contract.ts
+++ src/types/contract.ts
@@ -1,3 +1,9 @@
+export interface RoundDetails {
+  startTime: number
+  endTime: number
+  metadataUri: string
+}
+
 export interface UserBalances {
   claimableTokens: string
   allowedAmount: string
@@ -7,6 +13,6 @@ export interface ContractState {
   isConnected: boolean
   isOwner: boolean
   address: string | null
-  roundDetails: null
+  roundDetails: RoundDetails | null
   userBalances: UserBalances | null
 }
