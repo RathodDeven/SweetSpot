df93fcdb0854ead4078b9c53a8d2dcbfdec6c8df
RathodDeven <devenrathodrd@gmail.com>
2025-03-23 12:36:30 +0000

    feat: add IS_FIXED_THEME constant to config and update ThemeProvider for fixed theme handling

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,7 @@ export const APP_NAME = 'SweetSpot'
 export const IPFS_ENDPOINT = 'https://4everland.io/ipfs/'
 export const IMAGE_KIT_ENDPOINT = 'https://ik.imagekit.io/kopveel8c'
 export const DEFAULT_THEME: 'light' | 'dark' = 'light'
+export const IS_FIXED_THEME = true
 
 export const EVER_REGION = 'us-west-2'
 export const EVER_ENDPOINT = 'https://endpoint.4everland.co'
